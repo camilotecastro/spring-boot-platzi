ca38676c75e2e5d42a4bf7821f03a2ee79b3e3f5
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-22 13:05:32 -0500

    Se creo el metodo getInformationJpqlFromUser para ejecutar la sentencia findByUserEmail

--- src/main/java/com/camilo/springbootplatzi/SpringBootPlatziApplication.java
+++ src/main/java/com/camilo/springbootplatzi/SpringBootPlatziApplication.java
@@ -52,22 +52,33 @@ public class SpringBootPlatziApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
        //ejemplosAnteriores();
        saveUsersInDataBase();
+       getInformationJpqlFromUser();
+    }
+
+    public void getInformationJpqlFromUser(){
+        String email = "nano@gmail.com";
+        // Mostramos la información con un log
+        LOGGER.info("Usuario con el metodo findByUserEmail: " +
+                userRepository
+                        .findByUserEmail(email)
+                        .orElseThrow(()->
+                                new RuntimeException("No se encontro ningun usuario con el email: " + email)));
     }
 
     public void saveUsersInDataBase(){
-        User user = new User("Jhon","jhon@gmail.com", LocalDate.of(2021,03,1));
-        User user1 = new User("Julie","jhon@gmail.com", LocalDate.of(2021,11,1));
-        User user2 = new User("Daniela","jhon@gmail.com", LocalDate.of(2021,12,2));
-        User user3 = new User("user3","jhon@gmail.com", LocalDate.of(2021,03,4));
-        User user4 = new User("user4","jhon@gmail.com", LocalDate.of(2021,03,6));
-        User user5 = new User("user5","jhon@gmail.com", LocalDate.of(2021,04,11));
-        User user6 = new User("user6","jhon@gmail.com", LocalDate.of(2021,04,30));
-        User user7 = new User("user7","jhon@gmail.com", LocalDate.of(2021,05,11));
-        User user8 = new User("user8","jhon@gmail.com", LocalDate.of(2021,03,21));
-        User user9 = new User("user9","jhon@gmail.com", LocalDate.of(2021,05,23));
-        User user10 = new User("user10","jhon@gmail.com", LocalDate.of(2021,06,24));
-        User user11 = new User("user11","jhon@gmail.com", LocalDate.of(2021,06,24));
-        User user12 = new User("user12","jhon@gmail.com", LocalDate.of(2021,07,11));
+        User user = new User("Jhon","jhon1@gmail.com", LocalDate.of(2021,03,1));
+        User user1 = new User("Julie","Julie@gmail.com", LocalDate.of(2021,11,1));
+        User user2 = new User("Daniela","Daniela@gmail.com", LocalDate.of(2021,12,2));
+        User user3 = new User("user3","user3@gmail.com", LocalDate.of(2021,03,4));
+        User user4 = new User("user4","user4@gmail.com", LocalDate.of(2021,03,6));
+        User user5 = new User("user5","user5@gmail.com", LocalDate.of(2021,04,11));
+        User user6 = new User("user6","user6@gmail.com", LocalDate.of(2021,04,30));
+        User user7 = new User("user7","jhuser7on@gmail.com", LocalDate.of(2021,05,11));
+        User user8 = new User("user8","user8@gmail.com", LocalDate.of(2021,03,21));
+        User user9 = new User("user9","jhuser9on@gmail.com", LocalDate.of(2021,05,23));
+        User user10 = new User("user10","user10@gmail.com", LocalDate.of(2021,06,24));
+        User user11 = new User("user10","jhuser10on@gmail.com", LocalDate.of(2021,06,24));
+        User user12 = new User("user12","user12@gmail.com", LocalDate.of(2021,07,11));
 
         List<User> users = Arrays.asList(
                 user,user1,user2,user3,user4,user5,user6,user7,user8,user9,user10,user11,user12
@@ -78,7 +89,7 @@ public class SpringBootPlatziApplication implements CommandLineRunner {
     public void ejemplosAnteriores(){
         componentDependency.saludar();
         myBean.print();
-        myBeanWithDependency.printWithDepedency();
+        myBeanWithDependency.printWithDependency();
         System.out.println(myBeanWithProperties.function());
         System.out.println(userPojo.getEmail() + " " + userPojo.getPassword());
         try {
