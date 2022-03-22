967bf2f9ef2e0925437d04bcec26c77eb904caaf
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-22 13:04:38 -0500

    Se creo la consulta JPQL Encontrar un usuario por email que envia el cliente por parametro

--- src/main/java/com/camilo/springbootplatzi/repository/UserRepository.java
+++ src/main/java/com/camilo/springbootplatzi/repository/UserRepository.java
@@ -2,8 +2,15 @@ package com.camilo.springbootplatzi.repository;
 
 import com.camilo.springbootplatzi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // u es el alias del usuario
+    // ?1 es el parametro
+    @Query("select u from User u where u.email =?1")
+    Optional<User> findByUserEmail(String userEmail);
 }
