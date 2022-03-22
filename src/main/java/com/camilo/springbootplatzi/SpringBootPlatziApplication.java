5f0b06419bb53387e901b54f4cf9082ef45d73ff
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-21 19:24:51 -0500

    Initial commit

---
+++ src/main/java/com/camilo/springbootplatzi/SpringBootPlatziApplication.java
@@ -0,0 +1,13 @@
+package com.camilo.springbootplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlatziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPlatziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/camilo/springbootplatzi/SpringBootPlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camilo.springbootplatzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlatziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
