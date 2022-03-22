a9384dc27f2c5942ae3a3066253ea32df3477a94
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-22 13:29:27 -0500

    Se agrego una forma diferente de hacer la consulta findByEmail. Se creo la consulta JPQL findAndSort que recibe un parametro el string que queremos buscar dentro de la columna name de nuestra tabla user y ademas los ordenamos de manera descendiente por id.

--- src/main/java/com/camilo/springbootplatzi/repository/UserRepository.java
+++ src/main/java/com/camilo/springbootplatzi/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.camilo.springbootplatzi.repository;
 
 import com.camilo.springbootplatzi.entity.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // ?1 es el parametro
     @Query("select u from User u where u.email =?1")
     Optional<User> findByUserEmail(String userEmail);
+    // otra manera de hacer la misma Query
+    @Query
+    Optional<User> findByEmail(String userEmail);
+    // Selecciona todos los usuarios que inicen con un string dado por parametro
+    // y los ordena de la manera en que yo quiera ya sea de forma descendente o ascendente
+    @Query("select u from User u where u.name like ?1%")
+    List<User> findAndSort(String userName, Sort sort);
 }
