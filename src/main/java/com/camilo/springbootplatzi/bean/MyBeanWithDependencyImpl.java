7c1f4baabefa6f331c3cf82e2b1b7b7ce9f6fbe1
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-21 23:42:45 -0500

    Se agregaron unos logger para ver info debugg y error desde la consola

--- src/main/java/com/camilo/springbootplatzi/bean/MyBeanWithDependencyImpl.java
+++ src/main/java/com/camilo/springbootplatzi/bean/MyBeanWithDependencyImpl.java
@@ -1,8 +1,11 @@
 package com.camilo.springbootplatzi.bean;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MyBeanWithDependencyImpl implements MyBeanWithDependency {
+    Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImpl.class);
     private final MyOperation myOperation;
 
     @Autowired
@@ -12,7 +15,10 @@ public class MyBeanWithDependencyImpl implements MyBeanWithDependency {
 
     @Override
     public void printWithDepedency() {
-        System.out.println(myOperation.sum(2));
+        LOGGER.info("Entramos al metodo printWithDependency");
+        int num = 1;
+        System.out.println(myOperation.sum(num));
+        LOGGER.debug("El numero enviado es "+ num);
         System.out.println("hola desde la implementacion de My bean with dependency");
     }
 }
