c7d653117a4db473f9b20f6473f81d40f3226e49
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-22 20:09:53 -0500

    Se agregaron tres metodos update delete and save user.

--- src/main/java/com/camilo/springbootplatzi/service/UserService.java
+++ src/main/java/com/camilo/springbootplatzi/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -29,5 +30,25 @@ public class UserService {
 
     public List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public User update(User userUpdate, Long userId) {
+        return userRepository.findById(userId)
+                .map(
+                        user -> {
+                            user.setEmail(userUpdate.getEmail());
+                            user.setName(userUpdate.getName());
+                            user.setBirthDate(userUpdate.getBirthDate());
+                            return userRepository.save(user);
+                        }
+                ).get();
     }
 }
